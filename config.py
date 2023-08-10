9807f7d9a2d8d8f49c9379ed9e5df942182ce7ff
Alone <34532101+alonelove7@users.noreply.github.com>
2023-08-10 14:02:32 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "a393f2662e64e1d68bfa2e5f90b4db72",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -24,4 +24,4 @@ MODES = {
 # TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "a393f2662e64e1d68bfa2e5f90b4db72"
